a924a133857472680d8c6bcfd1d27b17f6d1b28d
Jason Mitchell <foobardog@hotmail.com>
2016-08-02 13:51:58 -0700

    Wiring up Brute
    We're not really using brute to find our components, though. This is mainly because CLJS kind of messes with the type system in a way that's hard.

--- src/fossa/core.cljs
+++ src/fossa/core.cljs
@@ -9,31 +9,85 @@
 
 (enable-console-print!)
 
-(defonce *phzr-game* (atom nil))
-(defonce *systems-state* (atom nil))
+(defonce *system* (atom nil))
 
-(defn- preload-game [game]
-  (let [loader (:load game)]
-    (doto loader
-      (p.loader/image "background" "assets/images/background.png")
-      (p.loader/image "mouse" "assets/images/mouse.png"))))
+(defprotocol Asset
+  (asset-type [this] "Returns a string describing the asset type")
+  (asset-key [this] "Returns a key to be used to access the asset.")
+  (preload-asset [this system loader] "Called during the preload stage"))
 
-(defn- create-game [game]
-  (let [factory (:add game)
-        world (:world game)]
-    (p.factory/sprite factory 0 0 "background")
-    (p.factory/sprite factory 344 264 "mouse")))
+(defrecord Image [asset-key path]
+  Asset
+  (asset-type [_] "image")
+  (asset-key [_] asset-key)
+  (preload-asset [_ _ loader]
+    (p.loader/image loader asset-key path)))
 
-(defn- update-game [game])
+(defprotocol Component
+  (get-assets [this system] "Returns a seq of assets necessary for this component")
+  (create-component [this system] "Called during the create stage")
+  (update-component [this system] "Called during the update stage"))
 
-(defn init-page! []
-  (when-let [current-game @*phzr-game*]
-    (p.game/destroy current-game))
-  (reset! *phzr-game* (p.game/->Game 768 576 (p.core/phaser-constants :auto) "phzr-game"
-                                     {"preload" preload-game
-                                      "create" create-game
-                                      "update" update-game})))
+(defrecord Sprite [initial-x initial-y asset-key image-path]
+  Component
+  (get-assets [_ _] [(->Image asset-key image-path)])
+  (create-component [_ system]
+    (let [factory (-> system :game :add)]
+      (p.factory/sprite factory initial-x initial-y asset-key)))
+  (update-component [_ system] system))
 
-(init-page!)
+(defn preload-assets! [game]
+  (let [system @*system*
+        loader (-> system :game :load)]
+    (doseq [entity (b.entity/get-all-entities system)]
+      (doseq [component (b.entity/get-all-components-on-entity system entity)]
+        (doseq [asset (get-assets component system)]
+            (preload-asset asset system loader))))
+    (reset! *system* system)))
+
+(defn create-entity-components! [game]
+  (let [system @*system*]
+    (doseq [entity (b.entity/get-all-entities system)]
+      (doseq [component (b.entity/get-all-components-on-entity system entity)]
+        (create-component component system)))
+    (reset! *system* system)))
+
+(defn update-entity-components! [game]
+  (let [system @*system*]
+    (doseq [entity (b.entity/get-all-entities system)]
+      (doseq [component (b.entity/get-all-components-on-entity system entity)]
+        (update-component component system)))
+    (reset! *system* system)))
+
+(def game-width 768)
+(def game-height 576)
+(defn get-new-game-object []
+  (p.game/->Game
+    game-width
+    game-height
+    (p.core/phaser-constants :auto)
+    "phzr-game"
+    {"preload" preload-assets!
+     "create" create-entity-components!
+     "update" update-entity-components!}))
+
+(defn define-entities [system]
+  (let [mouse (b.entity/create-entity)
+        background (b.entity/create-entity)]
+    (-> system
+        (b.entity/add-entity background)
+        (b.entity/add-component background (->Sprite 0 0 "background" "assets/images/background.png"))
+        (b.entity/add-entity mouse)
+        (b.entity/add-component mouse (->Sprite 344 264 "mouse" "assets/images/mouse.png")))))
+
+(defn create-system! []
+  (when-let [curr-game (:game @*system*)]
+    (p.game/destroy curr-game))
+  (-> (b.entity/create-system)
+      (assoc :game (get-new-game-object))
+      (define-entities)
+      (as-> s (reset! *system* s))))
+
+(create-system!)
 
 (defn on-js-reload [])
